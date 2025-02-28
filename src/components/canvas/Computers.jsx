611adf482333a956394ac82003702a952d3e2361
Its-me-ravi <rp808462@gmail.com>
2025-02-28 13:35:16 +0530

    updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -19,10 +19,16 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
-      <primitive
+      {/* <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      /> */}
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 1.0 : 0.75}
+        position={isMobile ? [0, -2, -1.5] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -54,12 +60,18 @@ const ComputersCanvas = () => {
   }, []);
 
   return (
+    // <Canvas
+    //   frameloop='demand'
+    //   shadows
+    //   dpr={[1, 2]}
+    //   camera={{ position: [20, 3, 5], fov: 25 }}
+    //   gl={{ preserveDrawingBuffer: true }}
+    // >
     <Canvas
-      frameloop='demand'
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      frameloop="always"
+      dpr={[1, 1.5]}
+      shadows={false}
+      camera={{ position: [10, 2, 5], fov: 30 }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -67,7 +79,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile ? [0, -2, -1.5] : [0, -3.25, -1.5]} />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
