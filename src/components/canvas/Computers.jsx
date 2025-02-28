ed0c2d70bc1b071731f14b5402d29363b0685379
Its-me-ravi <rp808462@gmail.com>
2025-03-01 00:49:52 +0530

    Updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,29 +8,24 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    // <mesh>
-    //   <hemisphereLight intensity={0.15} groundColor='black' />
-    //   <spotLight
-    //     position={[-20, 50, 10]}
-    //     angle={0.12}
-    //     penumbra={1}
-    //     intensity={1}
-    //     castShadow
-    //     shadow-mapSize={1024}
-    //   />
-    //   <pointLight intensity={1} />
-    //   <primitive
-    //     object={computer.scene}
-    //     scale={isMobile ? 0.7 : 0.75}
-    //     position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-    //     rotation={[-0.01, -0.2, -0.1]}
-    //   />
-    // </mesh>
     <mesh>
-  <boxGeometry args={[1, 1, 1]} />
-  <meshStandardMaterial color="red" />
-</mesh>
-
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   );
 };
 
@@ -59,12 +54,22 @@ const ComputersCanvas = () => {
   }, []);
 
   return (
+    // <Canvas
+    //   frameloop='demand'
+    //   shadows
+    //   dpr={[1, 2]}
+    //   camera={{ position: [20, 3, 5], fov: 25 }}
+    //   gl={{ preserveDrawingBuffer: true }}
+    // >
     <Canvas
-      frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      camera={{ position: [5, 2, 5], fov: 50 }}
+      gl={{
+        preserveDrawingBuffer: true,
+        antialias: true,
+        powerPreference: "high-performance",
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
